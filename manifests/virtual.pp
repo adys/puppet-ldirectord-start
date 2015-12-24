36887444d93a8b48915d79707c855104ce2742f6
Andrej Krnac <andrej.krnac@websupport.sk>
2015-12-24 12:09:34 +0100

    Create ldirectord_start module

--- manifests/virtual.pp
+++ manifests/virtual.pp
@@ -6,12 +6,12 @@ define ldirectord_start::virtual(
 ) {
   notify{"LDIRECTORD PROFILE ${title} ${$title}": } 
 
-  if ! defined(Class['ldirectord_start']) {
-    include ldirectord_start
-  }
+  #if ! defined(Class['ldirectord_start']) {
+  #  include ldirectord_start
+  #}
   
   concat::fragment { 'test':
-    target  => $ldirectord_start::configfile,
+    target  => $ldirectord_start::config,
     content => template('ldirectord_start/ldirectord_start.virtual.cf.erb'),
     order   => 'test',
   }
