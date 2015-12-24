1cc99d5a1f5206d8ab69b188c2f3a3ae965d25c4
Andrej Krnac <andrej.krnac@websupport.sk>
2015-12-24 09:34:40 +0100

    Create ldirectord_start module

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,4 +7,10 @@ class ldirectord_start::config inherits ldirectord_start {
     mode   => '0644',
   }
 
+  concat::fragment { 'global_opts':
+    target  => $ldirectord_start::config,
+    content => template('ldirectord_start/ldirectord_start.global.cf.erb'),
+    order   => '00',
+  }
+
 }
