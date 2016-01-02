3b27acb3f355dcea7261d03c255a9bd50af6baed
Andrej Krnac <andrej.krnac@websupport.sk>
2016-01-02 16:22:22 +0100

    Refactoring

--- manifests/config.pp
+++ manifests/config.pp
@@ -39,10 +39,10 @@ class ldirectord_start::config {
 
   #fork
 
-  if ($quiescent != 'yes') or ($quiescent == 'no') {
+  if ($quiescent != 'yes') and ($quiescent == 'no') {
     fail('$quiescent must have value "yes" or "no".')
   }
-  if ($cleanstop != 'yes') or ($cleanstop == 'no') {
+  if ($cleanstop != 'yes') and ($cleanstop == 'no') {
     fail('$cleanstop must have value "yes" or "no".')
   }
   if ($maintenancedir != undef) {
