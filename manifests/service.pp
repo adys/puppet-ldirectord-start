f049e0222e6b9e7dbfdd0b648277bd353d69e02b
Andrej Krnac <andrej.krnac@websupport.sk>
2016-01-05 20:39:42 +0100

    Fix Typo

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,7 +4,7 @@ class ldirectord_start::service (
   $service_name    = 'ldirectord',
 ) {
 
-  service { 'nginx':
+  service { 'ldirectord':
     ensure     => $service_ensure,
     name       => $service_name,
     enable     => $service_enable,
